dd6236c2dd9d2d21fd9e9f30e0d9caa5bac7121b
Zamerot <JasonSCrowley@iinet.net.au>
2016-04-05 20:37:53 +0800

    3D Boxes rendering. Camera needs to be figured out

---
+++ dev/src/main/java/dev/guitest/Boxes.java
@@ -0,0 +1,7 @@
+package dev.guitest;
+
+/**
+ * Created by Jason on 5/04/2016.
+ */
+public class Boxes {
+}
---
+++ dev/src/main/java/dev/guitest/MainWindow.java
@@ -0,0 +1,18 @@
+package dev.guitest;/**
+ * Created by Jason on 5/04/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
