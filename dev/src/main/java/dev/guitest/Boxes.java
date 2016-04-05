30e7801972cb8b26ff2d3583bc0b91a4c1a8cee6
Zamerot <JasonSCrowley@iinet.net.au>
2016-04-05 20:48:29 +0800

    3D Changes Camera needs work

--- dev/src/main/java/dev/guitest/Boxes.java
+++ dev/src/main/java/dev/guitest/Boxes.java
@@ -1,7 +1,128 @@
 package dev.guitest;
 
+import dev.business.Item;
+import dev.business.StripBin;
+import javafx.geometry.Point3D;
+import javafx.scene.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+
+import java.util.List;
+
 /**
  * Created by Jason on 5/04/2016.
  */
-public class Boxes {
+public class Boxes extends Group {
+
+    private Camera camera;
+
+    public Boxes(List<StripBin> boxes, double width, double length)
+    {
+        Box mainBox = new Box(width, 10, length);
+        mainBox.setMaterial(new PhongMaterial(Color.GREY));
+
+        Group boxesGroup = new Group();
+//        boxesGroup.getChildren().add(mainBox);
+
+
+
+
+
+
+        Color currentColor = Color.RED;
+
+        double transLength = 0;
+        double transWidth = 0;
+        double transHeight = mainBox.getHeight();
+
+        double widest = 0;
+
+        for(StripBin bin : boxes)
+        {
+            for (Item item : bin.getItems())
+            {
+                Box tempBox = new Box(item.getWidth(), item.getHeight(), item.getLength());
+
+                currentColor = determineCurrent(currentColor);
+                tempBox.setMaterial(new PhongMaterial(currentColor));
+                tempBox.setTranslateY(transHeight);
+                tempBox.setTranslateX(transWidth);
+                tempBox.setTranslateZ(transLength);
+
+
+                boxesGroup.getChildren().add(tempBox);
+
+                transLength += tempBox.getDepth();
+
+                if(tempBox.getWidth() > widest)
+                {
+                    widest = tempBox.getWidth();
+                }
+            }
+
+            transWidth += widest;
+
+            transLength = 0;
+        }
+//
+
+//        camera.getTransforms().addAll (
+//                new Rotate(-20, Rotate.Y_AXIS),
+//                new Rotate(-20, Rotate.X_AXIS),
+//                new Translate(-width/2,0, -50));
+
+
+        camera = new PerspectiveCamera(false);
+
+        camera.setTranslateY(-500);
+        camera.setTranslateX(-150);
+        camera.setTranslateZ(300);
+
+//        camera = new PerspectiveCamera(true);
+//        camera.setFieldOfView(62);
+//        camera.setTranslateY(-150);
+//        camera.setTranslateX(-250);
+//        camera.setTranslateZ(-100);
+//        camera.setTranslateY(-5);
+
+        Rotate rotateY;
+        Rotate rotateX;
+        Rotate rotateZ;
+        Translate translate;
+
+
+//        camera.getTransforms().addAll(
+//                rotateY = new Rotate(0, Rotate.Y_AXIS),
+//                rotateX = new Rotate(0, Rotate.X_AXIS),
+//                rotateZ = new Rotate(0, Rotate.Z_AXIS),
+//                translate = new Translate(50, 0, 90));
+
+        Group root = new Group();
+        root.getChildren().add(camera);
+        root.getChildren().add(boxesGroup);
+
+        SubScene subScene;
+        subScene = new SubScene(root, 600, 600, true, SceneAntialiasing.DISABLED);
+        subScene.setCamera(camera);
+
+        this.getChildren().add(subScene);
+    }
+
+
+    private Color determineCurrent(Color c)
+    {
+        if(c.equals(Color.RED))
+        {
+            return Color.GREEN;
+        }
+        else if(c.equals(Color.GREEN))
+        {
+            return Color.RED;
+        }
+
+        return c;
+    }
 }
--- dev/src/main/java/dev/guitest/MainWindow.java
+++ dev/src/main/java/dev/guitest/MainWindow.java
@@ -2,8 +2,17 @@ package dev.guitest;/**
  * Created by Jason on 5/04/2016.
  */
 
+import dev.business.GAProcessor;
+import dev.business.StripBin;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 public class MainWindow extends Application {
 
@@ -11,8 +20,30 @@ public class MainWindow extends Application {
         launch(args);
     }
 
+
+    private Pane createContent()
+    {
+        VBox mainBox = new VBox();
+
+        mainBox.getChildren().add(new Label("Label HERE"));
+
+
+        GAProcessor processor = new GAProcessor();
+        List<StripBin> best = processor.finBest(GAProcessor.sampleData(200), 200, 100);
+
+        mainBox.getChildren().add(new Boxes(best, 200, 100));
+
+        return mainBox;
+    }
+
+
     @Override
     public void start(Stage primaryStage) {
 
+        primaryStage.setTitle("Main App");
+
+        Scene scene = new Scene(createContent());
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
